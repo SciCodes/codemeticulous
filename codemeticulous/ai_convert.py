27c6edf9fbd0c4a77059c3dac5e24a4fb564ec7f
Kelly <kellynhitran@gmail.com>
2025-11-25 09:48:00 -0700

    fix(WIP): improve "poor" llm model performance
    - less capable models like mistral-nemo are failing to map cirtical fields.
    - chunking doesn't seem to improve this, might try  refining C-o-T prompt

--- codemeticulous/ai_convert.py
+++ codemeticulous/ai_convert.py
@@ -3,42 +3,13 @@ from codemeticulous.standards import STANDARDS
 from litellm import completion
 import logging
 import instructor
+from codemeticulous.prompt_strategies import FewShotStrategy
 
-console = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 # Toggle for additional llm debugging
 # litellm._turn_on_debug()
 
-INSTRUCTION_PROMPT = """
-You are a metadata conversion specialist. Your task is to convert source metadata from one format to another using the provided schemas.
-
-INPUTS PROVIDED:
-- Source data: A Pydantic model instance containing the original metadata
-
-INSTRUCTIONS:
-1. Analyze the source data and understand its structure.
-2. Map the relevant fields from the source data to the corresponding fields in the target response format.
-3. Transform data types and structures as needed to match the target schema requirements which could either be one-to-one or complex transformations.
-4. Instantiate the target model using the mapped and transformed data, so that a new instance of the target Pyndantic model can be created.
-
-OUTPUT REQUIREMENTS:
-- Return ONLY a valid JSON object that conforms exactly to the target response schema
-- Do not include any explanatory text, comments, or additional formatting
-- Ensure all required fields in the target response schema are populated and in order.
-- Use appropriate data types as defined in the target response schema
-
-The final output must be an instance of the target response schema that can be successfully validated by Pydantic.
-"""
-
-def generate_prompts(source_instance, target_model: BaseModel) -> list:
-    target_schema = target_model.schema_json(indent=2)
-    logging.info("TARGET SCHEMA: " + target_model.__name__)
-    logging.info(f"Schema length: {len(target_schema)} characters")
-
-    return [
-        {"role": "system", "content": INSTRUCTION_PROMPT},
-        {"role": "user", "content": "SOURCE_DATA:\n" + source_instance.json()},
-    ]
 
 def structured_completion(llm_model: str, messages: list, target_model: BaseModel, key: str) -> BaseModel | None:
     client = instructor.from_litellm(completion)
@@ -79,7 +50,8 @@ def convert_ai(key: str, llm_model: str, source_format: str, target_format: str,
     elif isinstance(source_data, source_model):
         source_instance = source_data
 
-    messages = generate_prompts(source_instance, target_model)
+    strategy = FewShotStrategy()
+    messages = strategy.generate_system_prompt(source_instance)
 
     # TESTING: instructor structured output
     target_data = structured_completion(llm_model, messages, target_model, key)
